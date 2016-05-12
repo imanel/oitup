497283c4839f71273873f6d2b2f72c946890ed6f
Bernard Potocki <bernard.potocki@imanel.org>
2016-05-12 09:31:12 +0200

    Base downloader class

--- application.js
+++ application.js
@@ -1,8 +1,9 @@
-var createAlert;
+var Downloader, createAlert, escapeHTML;
 
 App.onLaunch = function(options) {
-  var alert;
-  alert = createAlert('Put.IO Token', localStorage.getItem('putioAccessToken'));
+  var alert, downloader;
+  downloader = new Downloader(localStorage.getItem('putioAccessToken'));
+  alert = createAlert('Put.IO Token', downloader.urlForList());
   return navigationDocument.pushDocument(alert);
 };
 
@@ -18,7 +19,48 @@ App.onWillTerminate = function() {};
 
 createAlert = function(title, description) {
   var alertString, parser;
-  alertString = "<?xml version='1.0' encoding='UTF-8' ?>\n<document>\n  <alertTemplate>\n    <title>" + title + "</title>\n    <description>" + description + "</description>\n  </alertTemplate>\n</document>";
+  alertString = "<?xml version='1.0' encoding='UTF-8' ?>\n<document>\n  <alertTemplate>\n    <title>" + (escapeHTML(title)) + "</title>\n    <description>" + (escapeHTML(description)) + "</description>\n  </alertTemplate>\n</document>";
   parser = new DOMParser();
   return parser.parseFromString(alertString, 'application/xml');
 };
+
+escapeHTML = function(string) {
+  return String(string).replace(/[\"&<>]/g, function(chr) {
+    return {
+      '"': '&quot;',
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;'
+    }[chr];
+  });
+};
+
+Downloader = (function() {
+  function Downloader(accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  Downloader.prototype.baseURL = 'https://api.put.io/v2';
+
+  Downloader.prototype.url = function(path) {
+    var operator;
+    operator = path.indexOf('?') > -1 ? '&' : '?';
+    return this.baseURL + path + operator + 'oauth_token=' + this.accessToken;
+  };
+
+  Downloader.prototype.urlForList = function(parentId) {
+    var path;
+    path = '/files/list';
+    if (parentId) {
+      path += '?parent_id=' + parentId;
+    }
+    return this.url(path);
+  };
+
+  Downloader.prototype.urlForMovie = function(fileId) {
+    return this.url("/files/" + fileId + "/hls/media.m3u8?subtitle_key=all");
+  };
+
+  return Downloader;
+
+})();
--- src/application.coffee
+++ src/application.coffee
@@ -1,5 +1,6 @@
 App.onLaunch = (options) ->
-  alert = createAlert('Put.IO Token', localStorage.getItem('putioAccessToken'))
+  downloader = new Downloader localStorage.getItem('putioAccessToken')
+  alert = createAlert 'Put.IO Token', downloader.urlForList()
   navigationDocument.pushDocument alert
 
 App.onWillResignActive = ->
@@ -17,10 +18,30 @@ createAlert = (title, description) ->
      <?xml version='1.0' encoding='UTF-8' ?>
      <document>
        <alertTemplate>
-         <title>#{ title }</title>
-         <description>#{ description }</description>
+         <title>#{ escapeHTML title }</title>
+         <description>#{ escapeHTML description }</description>
        </alertTemplate>
      </document>
   """
   parser = new DOMParser()
   parser.parseFromString(alertString, 'application/xml')
+
+escapeHTML = (string) ->
+  String(string).replace /[\"&<>]/g, (chr) -> { '"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;' }[chr]
+
+class Downloader
+  constructor: (@accessToken) ->
+
+  baseURL: 'https://api.put.io/v2'
+
+  url: (path) ->
+    operator = if path.indexOf('?') > -1 then '&' else '?'
+    @baseURL + path + operator + 'oauth_token=' + @accessToken
+
+  urlForList: (parentId) ->
+    path = '/files/list'
+    path += '?parent_id=' + parentId if parentId
+    @url(path)
+
+  urlForMovie: (fileId) ->
+    @url("/files/#{ fileId }/hls/media.m3u8?subtitle_key=all")
